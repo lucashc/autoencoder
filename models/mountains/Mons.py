938e0b35cd657bdf041311f061861fe177980657
Sven Holtrop <yadeses@gmail.com>
2019-11-27 23:10:35 +0100

    Temp commit

---
+++ models/mountains/Mons.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class Mons(nn.Module):
+
+	def __init__(self):
+		super().__init__()
+		self.encoder = nn.Sequential(
+			nn.Conv2d(1, 32, 4, stride=4, padding=0), # (N x 32 x 128 x 128)
+			nn.MaxPool2d(2, stide=2), # (N x 32 x 64 x 64)
+			nn.Conv2d(32, 64, 4, stride=2, padding=0), # (N x 64 x 32 x 32)
+			nn.MaxPool(2, stride=2), # (N x 64 x 16 x 16)
+			nn.Conv2d(64, 128, 8, stride=8, padding=0), # (N x 128 x 2 x 2)
+			nn.MaxPool(2, stride=2) # (N x 128 x 1 x 1)
+		)
+		self.decoder = nn.Sequential(
+			nn.ConvTranspose2d(128, 64, 16, stride=16),
+			nn.ConvTranspose2d(64, 32, , stride=,padding=),
+			nn.ConvTranspose2d(32, 16, , stride=,padding=),
+			nn.ConvTranspose2d(16, 1, , stride=, padding=)
+		)
+
+	def forward(self, x):
+		return self.decoder(self.encoder(x))
