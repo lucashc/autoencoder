ecb5178ff4037b70a42b2e7347e0563d82802bfb
BuildTools <lucascrijns@gmail.com>
2019-11-28 11:35:26 +0100

    Added corruption detection

--- models/datasets/alps.py
+++ models/datasets/alps.py
@@ -50,11 +50,12 @@ TILES = ["n47e006",
 
 
 class Alps(Dataset):
-    def __init__(self, directory, download=False, resolution=512, in_memory=False, transform=None):
+    def __init__(self, directory, download=False, resolution=512, in_memory=False, transform=None, land_percentage=0.5):
         self.data_dir = directory
         self.tiles = os.path.join(directory, 'tiles/')
         self.npys = os.path.join(directory, 'npys/')
         self.resolution = resolution
+        self.land_percentage = land_percentage
         if not os.path.isfile(os.path.join(self.data_dir, 'data.lock')):
             if download:
                 os.makedirs(self.data_dir, exist_ok=True)
@@ -64,17 +65,20 @@ class Alps(Dataset):
                 self.download_files()
                 self.process()
             else:
-                raise FileNotFoundError("No lock file found")      
+                raise FileNotFoundError("No lock file found")
         self.filelist = sorted(os.listdir(self.npys), key=lambda f: int(f[:-4]))
         self.samples_per_file = (SAMPLES // self.resolution)**2
         self.samples_row_column = SAMPLES // self.resolution
         self.length = self.samples_per_file * len(self.filelist)
+        self.indices = list(range(self.length)) # All indices
 
         # Optional in memory storage
         self.in_memory = in_memory
         if self.in_memory:
             self.data = []
             self.load()
+        # Remove corrupt indices
+        self.mask_corrupted_indices()
         self.transform = transform
 
     def download_files(self):
@@ -90,6 +94,7 @@ class Alps(Dataset):
         print("Loading in memory")
         for file in tqdm(self.filelist):
             self.data.append(np.load(os.path.join(self.npys, file)))
+        print("Loaded")
 
     @staticmethod
     def unpack_file(filename):
@@ -97,6 +102,27 @@ class Alps(Dataset):
             elevations = np.fromfile(f, np.dtype('>i2'), SAMPLES*SAMPLES)\
                 .reshape((SAMPLES, SAMPLES))
         return elevations
+    
+    @staticmethod
+    def check_corruption(data, percentage=0.5):
+        # Corruption by definition if there are values smaller than 0, and otherwise by percentage of land area.
+	    return np.count_nonzero(data) < data.size*percentage or (data < 0).any()\
+    
+    def mask_corrupted_indices(self):
+        removals = 0
+        print("Pruning corrupted sectors")
+        for index in tqdm(self.indices.copy()):
+            file_index, tile_index, y, x = self.calculate_file_tile(index)
+            if self.in_memory:
+                sample = self.data[file_index][y[0]:y[1], x[0]:x[1]]
+            else:
+                sample = np.load(os.path.join(self.npys, self.filelist[file_index]))[y[0]:y[1], x[0]:x[1]]
+            if self.check_corruption(sample, self.land_percentage):
+                self.indices.remove(index)
+                removals += 1
+                tqdm.write(f"Corrupt sector at index {index}")
+        self.length -= removals
+        print(f"In total {removals} indices pruned")
     
     def process(self):
         print("Unpacking binary files...")
@@ -108,6 +134,15 @@ class Alps(Dataset):
     def __len__(self):
         return self.length
     
+    def calculate_file_tile(self, index):
+        file_index = index // self.samples_per_file
+        tile_index = index - file_index * self.samples_per_file
+        row = tile_index // self.samples_row_column
+        column = tile_index - self.samples_row_column * row
+        y = (self.resolution * row, self.resolution*(row+1))
+        x = (self.resolution * column, self.resolution*(column+1))
+        return file_index, tile_index, y, x
+    
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             index = idx.tolist()
@@ -115,12 +150,9 @@ class Alps(Dataset):
             index = idx
         if index >= self.length or index < 0:
             raise IndexError("Out of bounds")
-        file_index = index // self.samples_per_file
-        tile_index = index - file_index * self.samples_per_file
-        row = tile_index // self.samples_row_column
-        column = tile_index - self.samples_row_column * row
-        y = (self.resolution * row, self.resolution*(row+1))
-        x = (self.resolution * column, self.resolution*(column+1))
+        # Only get file from allowed indices
+        index = self.indices[index]
+        file_index, _, y, x = self.calculate_file_tile(index)
         if self.in_memory:
             sample = self.data[file_index][y[0]:y[1], x[0]:x[1]]
         else:
